a0ffccd98bf57b31bc412b1d7b9b48f2256e8c18
李礼彬 <llb0828@sina.com>
2016-03-04 10:43:40 +0800

    修改序列化方法

--- DF.Tryer.Tool/Helper/SendResquestHelper.cs
+++ DF.Tryer.Tool/Helper/SendResquestHelper.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Web.Script.Serialization;
 using Xfrog.Net;
 
 namespace DF.Tryer.Tool.Helper
@@ -36,6 +33,36 @@ namespace DF.Tryer.Tool.Helper
                 return newObj["error_code"].Value + ":" + newObj["reason"].Value;
             }
             return newObj.ToString();
+        }
+
+        /// <summary>
+        /// 调用数据访问方法返回结果
+        /// (Newtonsoft 转义问题未解决)
+        /// </summary>
+        /// <param name="key">appkey</param>
+        /// <param name="url">接口url</param>
+        /// <param name="parameter">参数</param>
+        /// <returns></returns>
+        public static string GetApiResult2(string key, string url, IDictionary<string, string> parameter)
+        {
+            var requestParameter = new Dictionary<string, string> { { "key", key }, { "dtype", "json" } };
+
+            foreach (var item in parameter)
+            {
+                requestParameter.Add(item.Key, item.Value);
+            }
+
+            var result = Network.SendGet(url, requestParameter);
+            var jobject = new JavaScriptSerializer().Deserialize<dynamic>(result);
+
+            var errorCode = jobject["error_code"].ToString();
+
+            // 失败
+            if (errorCode != "0")
+            {
+                return jobject["error_code"].ToString() + ":" + jobject["reason"].ToString();
+            }
+            return result;
         }
     }
 }
